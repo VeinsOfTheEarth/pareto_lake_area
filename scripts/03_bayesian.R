96644b3983db7bef227b1bb59d905d071b293c1b
jsta <stachel2@msu.edu>
2021-10-10 19:31:35 -0600

    increase iterations

--- scripts/03_bayesian.R
+++ scripts/03_bayesian.R
@@ -43,7 +43,7 @@ model {
 if (!file.exists("data/pareto_bayes.rds")) {
   fit <- stan(model_code = pareto_model,
     data = list(N = length(y_censored), x = y_censored),
-    iter = 15000)
+    iter = 25000)
 
   # print(fit)
   # plot(fit, pars = "alpha")
