686e6aa21b57342e99620f5b352abf4bf9438fee
jsta <stachel2@msu.edu>
2021-10-20 19:52:53 -0600

    fill out table1

---
+++ manuscript/tables/table_1.R
@@ -0,0 +1,31 @@
+source("scripts/utils.R")
+library(flextable)
+
+y_raw <- readRDS("data/y.rds")$y_raw
+total_empirical <- sum(inv_cumulative_freq(cumulative_freq(y_raw)))
+
+res <- readRDS("data/res.rds")
+total_predicted <- data.frame(
+  q50 = sum(inv_cumulative_freq(res)), 
+  q5 = sum(inv_cumulative_freq(
+    mutate(mutate(res, number_lower = coalesce(number_lower, number)), number = number_lower)
+    )),
+  q95 = sum(inv_cumulative_freq(
+    mutate(mutate(res, number_upper = coalesce(number_upper, number)), number = number_upper)
+    )) 
+  )
+
+area_bayes <- readRDS("data/area_bayes.rds")
+conf_int <- quantile(area_bayes, probs = c(0.025, 0.5, .975))
+
+res <- tibble::tribble(
+  ~ name,        ~ Q50, ~ Q5, ~ Q95,
+    "True",        total_empirical, NA, NA, 
+    "Frequentist", total_predicted$q50, total_predicted$q5, total_predicted$q95,
+    "Bayesian",    conf_int[2], conf_int[1], conf_int[3]
+)
+
+res[2,]$Q95 - res[2,]$Q5
+res[3,]$Q95 - res[3,]$Q5
+
+flextable(res)
