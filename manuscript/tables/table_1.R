1b61249299a760c52ebbd19ef3770fe450480818
jsta <stachel2@msu.edu>
2022-09-22 14:40:08 -0600

    linting

--- manuscript/tables/table_1.R
+++ manuscript/tables/table_1.R
@@ -11,32 +11,32 @@ total_predicted <- data.frame(
     mutate(
       mutate(res, number_lower = coalesce(number_lower, number)),
       number = number_lower)
-    )),
+  )),
   q95 = sum(inv_cumulative_freq(
     mutate(
       mutate(res, number_upper = coalesce(number_upper, number)),
       number = number_upper)
-    ))
-  )
+  ))
+)
 
 area_bayes <- readRDS("data/area_bayes.rds")
 conf_int <- quantile(area_bayes, probs = c(0.025, 0.5, .975))
 
-format_mil <- function(x){
+format_mil <- function(x) {
   round(x / 1000000, 3)
 }
 # format_mil(res[2,]$Q5)
 
-format_diff <- function(x){
+format_diff <- function(x) {
   round(x, 0)
 }
 # format_diff(conf_int[3] - conf_int[1])
 
 tibble::tribble(
-  ~ name,        ~ Q50, ~ Q5, ~ Q95, ~ Q95minusQ5,
-    "True",        format_mil(total_empirical), NA, NA, NA,
-    "Frequentist", format_mil(total_predicted$q50), format_mil(total_predicted$q5), format_mil(total_predicted$q95), format_diff(total_predicted$q95-total_predicted$q5),
-    "Bayesian",    format_mil(conf_int[2]), format_mil(conf_int[1]), format_mil(conf_int[3]), format_diff(conf_int[3]-conf_int[1])
+  ~name,        ~Q50, ~Q5, ~Q95, ~Q95minusQ5,
+  "True",        format_mil(total_empirical), NA, NA, NA,
+  "Frequentist", format_mil(total_predicted$q50), format_mil(total_predicted$q5), format_mil(total_predicted$q95), format_diff(total_predicted$q95 - total_predicted$q5),
+  "Bayesian",    format_mil(conf_int[2]), format_mil(conf_int[1]), format_mil(conf_int[3]), format_diff(conf_int[3] - conf_int[1])
 ) %>%
-flextable() %>%
-colformat_num(na_str = "-")
+  flextable() %>%
+  colformat_num(na_str = "-")
